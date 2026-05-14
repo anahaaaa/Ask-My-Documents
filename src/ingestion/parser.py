3fb3939eed7c3489e68e55081c179d29a4b98aad
anahaaaa <anagha11165@gmail.com>
2026-05-14 11:17:53 +0530

    feat : add PDF parsing and OCR ingestion pipeline

--- src/ingestion/parser.py
+++ src/ingestion/parser.py
@@ -1,10 +1,11 @@
 from unstructured.partition.auto import partition
+from utils.config import PDF_PARTITION_STRATEGY
 
 def partition_doc(file_path):
 
     elements = partition(
         filename=file_path,
-        strategy="hi_res",
+        strategy=PDF_PARTITION_STRATEGY,
         infer_table_structure=True,
         extract_image_block_types=['Image'],
         extract_image_block_to_payload=True
